8c9760ebff9a0a52d1e80f099419ae1f7bd5949f
Ganesh G <52821939+resurgence99@users.noreply.github.com>
2021-05-19 12:44:03 +0530

    minor changes

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -6,7 +6,13 @@ void main() {
   ),);
 }
 
-class NinjaApp extends StatelessWidget {
+class NinjaApp extends StatefulWidget {
+  @override
+  _NinjaAppState createState() => _NinjaAppState();
+}
+
+class _NinjaAppState extends State<NinjaApp> {
+  int c = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,11 +23,36 @@ class NinjaApp extends StatelessWidget {
         elevation: 0.0,
         backgroundColor: Colors.grey[900]
       ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            c += 1;
+          });
+        },
+        child: Icon(
+          Icons.add,
+        ),
+        backgroundColor: Colors.amber,
+      ),
+
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0,40.0,30.0,0.0),
 
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/IMG_1013.JPG'),
+                radius: 50.0,
+              ),
+            ),
+            Divider(
+              height: 50.0,
+              color: Colors.grey,
+            ),
+            SizedBox(height: 12,),
             Text('NAME',
             style: TextStyle(
               fontWeight: FontWeight.bold,
@@ -29,6 +60,7 @@ class NinjaApp extends StatelessWidget {
             ),
             ),
 
+            SizedBox(height: 5.0,),
             Text('Ganesh',
             style: TextStyle(
               fontWeight: FontWeight.bold,
@@ -37,6 +69,7 @@ class NinjaApp extends StatelessWidget {
             ),
             ),
 
+            SizedBox(height: 10.0,),
             Text('NINJA LEVEL',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
@@ -44,13 +77,29 @@ class NinjaApp extends StatelessWidget {
               ),
             ),
 
-            Text('5',
+            SizedBox(height: 5.0,),
+            Text('$c',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 color: Colors.amberAccent[700],
                 fontSize: 40.0,
               ),
             ),
+
+            SizedBox(height: 10.0,),
+            Row(
+              children: <Widget> [
+                Icon(Icons.mail,
+                color: Colors.grey,),
+                SizedBox(width: 15.0,),
+                Text('taresh.1999@gmail.com',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.amber,
+                  letterSpacing: 1.0,
+                ),)
+              ],
+            )
           ],
         ),
       )
